302f2552efde99ed32db7774ce7fa52c8a504504
CarlFlo <Carl.flodin@hotmail.com>
2021-03-06 12:22:59 +0100

    Added invite link

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -3,6 +3,8 @@ using Discord.WebSocket;
 using System;
 using System.Threading.Tasks;
 
+// Invite link: https://discordapp.com/oauth2/authorize?&client_id=817714291318849558&scope=bot&permissions=8
+
 namespace DiscordBot
 {
 	public class Program
