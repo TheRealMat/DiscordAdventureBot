9fba8feeb4f08abd0ba5df7d71491854f6e27513
Mat <mat555333@gmail.com>
2021-03-06 13:56:54 +0100

    Init

---
+++ DiscordBot/Bot.cs
@@ -0,0 +1,60 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+namespace DiscordBot
+{
+    class Bot
+    {
+        public DiscordClient client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json")){
+                using (var sr = new StreamReader(fs, new UTF8Encoding(false))){
+                    json = await sr.ReadToEndAsync().ConfigureAwait(false);
+                }
+            }
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
+            };
+
+            client = new DiscordClient(config);
+
+            client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableDms = false,
+                EnableMentionPrefix = true
+            };
+
+            Commands = client.UseCommandsNext(commandsConfig);
+
+            await client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(object sender, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,6 +1,4 @@
-﻿using Discord;
-using Discord.WebSocket;
-using System;
+﻿using System;
 using System.Threading.Tasks;
 
 // Invite link: https://discordapp.com/oauth2/authorize?&client_id=817714291318849558&scope=bot&permissions=8
@@ -9,26 +7,10 @@ namespace DiscordBot
 {
 	public class Program
 	{
-		private DiscordSocketClient _client;
-
-		public static void Main(string[] args)
-			=> new Program().MainAsync().GetAwaiter().GetResult();
-
-		public async Task MainAsync()
-		{
-			_client = new DiscordSocketClient();
-			_client.Log += Log;
-			await _client.LoginAsync(TokenType.Bot,
-				Environment.GetEnvironmentVariable("DiscordToken"));
-			await _client.StartAsync();
-
-			// Block this task until the program is closed.
-			await Task.Delay(-1);
-		}
-		private Task Log(LogMessage msg)
-		{
-			Console.WriteLine(msg.ToString());
-			return Task.CompletedTask;
-		}
+		static void Main(string[] args)
+        {
+			Bot bot = new Bot();
+			bot.RunAsync().GetAwaiter().GetResult();
+        }
 	}
 }
