36b002ac73d32b36e880d7d3fc7f961a7776497d
CarlFlo <Carl.flodin@hotmail.com>
2021-03-08 11:43:01 +0100

    Roll. Added upper limit to number of rolls and fixed formatting

--- DiscordBot/Commands/TestCommands.cs
+++ DiscordBot/Commands/TestCommands.cs
@@ -8,57 +8,68 @@ using System.Threading.Tasks;
 
 namespace DiscordBot.Commands
 {
-    class TestCommands : BaseCommandModule
-    {
-        [Command("test")]
-        [Description("Returns a message")]
-        public async Task Test(CommandContext ctx)
-        {
-            await ctx.Channel.SendMessageAsync("fuck you").ConfigureAwait(false);
-        }
+	class TestCommands : BaseCommandModule
+	{
+		[Command("test")]
+		[Description("Returns a message")]
+		public async Task Test(CommandContext ctx)
+		{
+			await ctx.Channel.SendMessageAsync("fuck you").ConfigureAwait(false);
+		}
 
-        [Command("powerlich")]
-        [Description("Returns a powerlich")]
-        public async Task Powerlich(CommandContext ctx)
-        {
-            await ctx.Channel.SendMessageAsync("<:powerlich:818391341163348008>").ConfigureAwait(false);
-        }
+		[Command("powerlich")]
+		[Description("Returns a powerlich")]
+		public async Task Powerlich(CommandContext ctx)
+		{
+			await ctx.Channel.SendMessageAsync("<:powerlich:818391341163348008>").ConfigureAwait(false);
+		}
 
-        [Command("emojis")]
-        [Description("Returns all emojis in guild")]
-        public async Task Emojis(CommandContext ctx)
-        {
-            IEnumerable<DiscordEmoji> emojis;
-            emojis = ctx.Guild.Emojis.Values;
-            string message = "";
+		[Command("emojis")]
+		[Description("Returns all emojis in guild")]
+		public async Task Emojis(CommandContext ctx)
+		{
+			IEnumerable<DiscordEmoji> emojis;
+			emojis = ctx.Guild.Emojis.Values;
+			string message = "";
 
-            foreach (DiscordEmoji emoji in emojis)
-            {
-                message += emoji.ToString();
-            }
+			foreach (DiscordEmoji emoji in emojis)
+			{
+				message += emoji.ToString();
+			}
 
-            await ctx.Channel.SendMessageAsync(message).ConfigureAwait(false);
-        }
+			await ctx.Channel.SendMessageAsync(message).ConfigureAwait(false);
+		}
 
-        [Command("roll")]
-        [Description("rolls custom dice")]
-        public async Task Roll(CommandContext ctx, [Description("Die face amount")] int faces, [Description("Die amount")] int number)
-        {
-            Random random = new Random();
-            string message = "";
-            int total = 0;
-            for (int i = 0; i < number; i++)
-            {
-                int result = random.Next(1, faces + 1);
-                total += result;
-                message += result.ToString();
-                message += ", ";
-            }
-            message += Environment.NewLine;
-            message += "Total: ";
-            message += total.ToString();
+		[Command("roll")]
+		[Description("rolls custom dice")]
+		public async Task Roll(CommandContext ctx, [Description("Die face amount")] int faces, [Description("Die amount")] int number)
+		{
 
-            await ctx.Channel.SendMessageAsync(message).ConfigureAwait(false);
-        }
-    }
+			// Will not throw more than 100 dices
+			if (number > 100)
+			{
+				number = 100;
+			}
+
+			Random random = new Random();
+			string message = "";
+			int total = 0;
+			for (int i = 0; i < number; i++)
+			{
+				int diceroll = random.Next(1, faces + 1);
+				total += diceroll;
+				message += diceroll.ToString();
+				// Formatting. The last ',' will be omitted from the output
+				if (i != number - 1)
+				{
+					message += ", ";
+				}
+			}
+			message += Environment.NewLine;
+			message += "Total: ";
+			message += total.ToString();
+
+			await ctx.Channel.SendMessageAsync(message).ConfigureAwait(false);
+		}
+	}
 }
