7642a1af11a8db14358bf26bc6327818ef156c1b
CarlFlo <39366974+CarlFlo@users.noreply.github.com>
2021-03-10 11:13:50 +0100

    Added discord permission calculator link

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading.Tasks;
 
+// Perm calculator https://discordapi.com/permissions.html#445504
 // Invite link: https://discordapp.com/oauth2/authorize?&client_id=817714291318849558&scope=bot&permissions=8
 
 namespace DiscordBot
